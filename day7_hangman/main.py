4ef895901da3c69fd34f84b7afd72b4c4d2e58d4
Mateusz Miernik <mateusz.miernik@aptiv.com>
2021-06-09 12:43:54 +0200

    Quick commit

--- day7_hangman/main.py
+++ day7_hangman/main.py
@@ -3,27 +3,24 @@
 """
 
 import random as rand
+from hangman_art import stages, logo, winner
+from hangman_words import word_list
+from replit import clear
 
-you_win = """
-██╗░░░██╗░█████╗░██╗░░░██╗  ░██╗░░░░░░░██╗██╗███╗░░██╗
-╚██╗░██╔╝██╔══██╗██║░░░██║  ░██║░░██╗░░██║██║████╗░██║
-░╚████╔╝░██║░░██║██║░░░██║  ░╚██╗████╗██╔╝██║██╔██╗██║
-░░╚██╔╝░░██║░░██║██║░░░██║  ░░████╔═████║░██║██║╚████║
-░░░██║░░░╚█████╔╝╚██████╔╝  ░░╚██╔╝░╚██╔╝░██║██║░╚███║
-░░░╚═╝░░░░╚════╝░░╚═════╝░  ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝
-"""
-
-words = ['mouse', 'parrot', 'hamster', 'giraffe', 'horse']
+game_is_finished = False
 
 if __name__ == "__main__":
+    print(logo)
     print(10*"-", "Welcome in the Python Hangman Game!", 10*"-")
-    word = rand.choice(words)
+    word = rand.choice(word_list)
     secret_word = ["_" for blank in range(len(word))]
-    lives = len(word)
+    lives = 6
 
-    while True:
+    print(stages[lives-1])
+    while not game_is_finished:
         print(f"\nYour word is {' '.join(letter for letter in secret_word)}")
         guess = input("Guess a letter for word: ").lower()
+        clear()
 
         if guess is not "" and guess in word and guess not in secret_word:
             print(f"Your guess is '{guess}'")
@@ -33,13 +30,15 @@ if __name__ == "__main__":
 
             print("You guessed a letter!")
             if "_" not in secret_word:
-                print(f"{you_win}\nCongratulations!")
+                print(f"{winner}\nCongratulations!")
                 print(f"Secret word was '{word}'.")
-                break
+                game_is_finished = True
         else:
             print("Wrong choice, You miss a life!")
             lives -= 1
             if lives == 0:
                 print("You lose! Game Over.")
                 print(f"Secret word was {word}")
-                break
+                game_is_finished = True
+
+        print(stages[lives])
