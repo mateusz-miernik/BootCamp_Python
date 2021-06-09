bb78e71246de3ecc461cc492de192a2555058b01
Mateusz Miernik <mateusz.miernik@aptiv.com>
2021-06-09 12:26:00 +0200

    Quick commit

--- day7_hangman/main.py
+++ day7_hangman/main.py
@@ -3,23 +3,38 @@
 """
 
 import random as rand
+
+you_win = """
+██╗░░░██╗░█████╗░██╗░░░██╗  ░██╗░░░░░░░██╗██╗███╗░░██╗
+╚██╗░██╔╝██╔══██╗██║░░░██║  ░██║░░██╗░░██║██║████╗░██║
+░╚████╔╝░██║░░██║██║░░░██║  ░╚██╗████╗██╔╝██║██╔██╗██║
+░░╚██╔╝░░██║░░██║██║░░░██║  ░░████╔═████║░██║██║╚████║
+░░░██║░░░╚█████╔╝╚██████╔╝  ░░╚██╔╝░╚██╔╝░██║██║░╚███║
+░░░╚═╝░░░░╚════╝░░╚═════╝░  ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝
+"""
+
 words = ['mouse', 'parrot', 'hamster', 'giraffe', 'horse']
 
 if __name__ == "__main__":
     print(10*"-", "Welcome in the Python Hangman Game!", 10*"-")
     word = rand.choice(words)
-    blanks = " ".join("_" for blank in range(len(word)))
+    secret_word = ["_" for blank in range(len(word))]
     lives = len(word)
 
     while True:
-        print(f"\nYour word is {blanks}")
+        print(f"\nYour word is {' '.join(letter for letter in secret_word)}")
         guess = input("Guess a letter for word: ").lower()
 
-        if guess in word:
-            blanks = blanks[:blanks.find(guess)] + guess + blanks[blanks.find(guess)+1:]
+        if guess is not "" and guess in word and guess not in secret_word:
+            print(f"Your guess is '{guess}'")
+            for idx, letter in enumerate(word):
+                if guess == letter:
+                    secret_word[idx] = guess
+
             print("You guessed a letter!")
-            if "_" not in blanks:
-                print("You win! Congratulations!")
+            if "_" not in secret_word:
+                print(f"{you_win}\nCongratulations!")
+                print(f"Secret word was '{word}'.")
                 break
         else:
             print("Wrong choice, You miss a life!")
