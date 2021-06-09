08dedea97514393127f5f932f8a621bfede3e7a6
Mateusz <mateusz.miernik1@gmail.com>
2021-06-09 21:02:03 +0200

    Quick commit

--- day7_hangman/main.py
+++ day7_hangman/main.py
@@ -3,14 +3,14 @@
 """
 
 import random as rand
-from hangman_art import stages, logo, winner
+from hangman_art import stages, logo, winner, loser
 from hangman_words import word_list
-from replit import clear
+import os
 
 game_is_finished = False
 
 if __name__ == "__main__":
-    print(logo)
+    print(logo, "\n")
     print(10*"-", "Welcome in the Python Hangman Game!", 10*"-")
     word = rand.choice(word_list)
     secret_word = ["_" for blank in range(len(word))]
@@ -20,25 +20,28 @@ if __name__ == "__main__":
     while not game_is_finished:
         print(f"\nYour word is {' '.join(letter for letter in secret_word)}")
         guess = input("Guess a letter for word: ").lower()
-        clear()
+        print(f"Your guess is '{guess}'")
+        os.system('cls')
 
-        if guess is not "" and guess in word and guess not in secret_word:
-            print(f"Your guess is '{guess}'")
+        if guess != "" and guess in word and guess not in secret_word:
+
             for idx, letter in enumerate(word):
                 if guess == letter:
                     secret_word[idx] = guess
 
             print("You guessed a letter!")
             if "_" not in secret_word:
-                print(f"{winner}\nCongratulations!")
+                print(f"\n{winner}\nCongratulations!")
                 print(f"Secret word was '{word}'.")
                 game_is_finished = True
         else:
             print("Wrong choice, You miss a life!")
             lives -= 1
             if lives == 0:
-                print("You lose! Game Over.")
-                print(f"Secret word was {word}")
+                print(f"\n{loser}\nGame Over.")
+                print(f"Secret word was '{word}'")
                 game_is_finished = True
 
         print(stages[lives])
+
+    exit_program = input("Press any key to exit.. ")
