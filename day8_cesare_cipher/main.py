0efb932e318fb8cdaa0af10fe4300b7789c15086
Mateusz Miernik <mateusz.miernik@aptiv.com>
2021-06-10 19:05:34 +0200

    Quick commit

--- day8_cesare_cipher/main.py
+++ day8_cesare_cipher/main.py
@@ -3,48 +3,48 @@
 """
 
 from string import ascii_lowercase
+from art import logo
 
 alphabet = [letter for letter in ascii_lowercase]
 
 
-def encrypt(message, shift_value):
-    encrypted_message = ""
+def caesar(message, shift_value, mode):
+    converted_message = ""
     new_alphabet = alphabet[shift_value:] + alphabet[:shift_value]
+
     for letter in message:
         if letter != " ":
-            idx = alphabet.index(letter)
-            encrypted_message += new_alphabet[idx]
+            try:
+                if mode == "encode":
+                    idx = alphabet.index(letter)
+                    converted_message += new_alphabet[idx]
+                else:
+                    idx = new_alphabet.index(letter)
+                    converted_message += alphabet[idx]
+            except ValueError:
+                print(f"Char '{letter}' is invalid! Skipping it..")
+                continue
         else:
-            encrypted_message += letter
+            converted_message += letter
 
-    print(f'Encrypted message is: {encrypted_message}')
-
-
-def decrypt(message, shift_value):
-    decrypted_message = ""
-    new_alphabet = alphabet[shift_value:] + alphabet[:shift_value]
-    for letter in message:
-        if letter != " ":
-            idx = new_alphabet.index(letter)
-            decrypted_message += alphabet[idx]
-        else:
-            decrypted_message += letter
-
-    print(f'Decrypted message is: {decrypted_message}')
+    print(f'Here is {mode}d result: {converted_message}')
 
 
 if __name__ == "__main__":
+    print(logo, "\n")
 
     while True:
         mode_type = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
         text = input("Type your message:\n").lower()
         shift = int(input("Type the shift number:\n"))
 
-        if mode_type == "encode":
-            encrypt(text, shift)
-        elif mode_type == "decode":
-            decrypt(text, shift)
+        if shift > len(alphabet):
+            shift = shift % len(alphabet)
+
+        if mode_type == "encode" or mode_type == "decode":
+            caesar(message=text, shift_value=shift, mode=mode_type)
         else:
+            print("You typed wrong mode type! Exiting..")
             break
 
         choice = input("Would you like to try again? Type 'yes' or 'no'\n")
