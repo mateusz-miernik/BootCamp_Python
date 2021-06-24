9161c054462ab1782913f73859d8737da07e2ec1
Mateusz Miernik <mateusz.miernik@aptiv.com>
2021-06-24 15:24:52 +0200

    Quick commit

---
+++ day15_coffee_machine/main.py
@@ -0,0 +1,21 @@
+"""
+    DAY 15: Coffee Machine
+"""
+
+
+def print_report():
+    pass
+
+
+def prepare_coffee():
+    pass
+
+
+def coffee_machine():
+    pass
+
+
+if __name__ == "__main__":
+    # print("☕")
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+
