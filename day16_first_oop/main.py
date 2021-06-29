4ddc0349308c3afb2bccd5e498cc42bde7661c6a
Mateusz Miernik <mateusz.miernik@aptiv.com>
2021-06-29 15:52:31 +0200

    Quick commit

---
+++ day16_first_oop/main.py
@@ -0,0 +1,14 @@
+"""
+    DAY 16: First OOP, turtle module.
+"""
+
+import turtle
+
+
+def main() -> None:
+    matthew = turtle.Turtle()
+    print(matthew)
+
+
+if __name__ == "__main__":
+    main()
