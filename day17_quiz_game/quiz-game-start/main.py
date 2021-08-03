b0d0d1f6631dcf83a69520c501d6f832b44bfc1d
Mateusz <mateusz.miernik1@gmail.com>
2021-08-04 00:08:12 +0200

    Quick commit

--- day17_quiz_game/quiz-game-start/main.py
+++ day17_quiz_game/quiz-game-start/main.py
@@ -4,13 +4,13 @@ from quiz_brain import QuizBrain
 import textwrap
 
 
-def wrap(s, w):
-    return textwrap.fill(s, w)
+def wrap(s: str, width: int):
+    return textwrap.fill(s, width)
 
 
 if __name__ == "__main__":
     question_bank = [Question(wrap(question["question"], 75), question["correct_answer"])
-                     for question in question_data['results']]
+                     for question in question_data]
     quiz = QuizBrain(question_bank)
 
     while quiz.still_has_questions():
