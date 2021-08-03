ea729a780f163498966d20d45cc6aeae4d4897e6
Mateusz <mateusz.miernik1@gmail.com>
2021-08-04 00:31:45 +0200

    Quick commit

---
+++ day18_turtle_graphics/exercises/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("triangle")
+timmy_the_turtle.color("green")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+
+
+screen = Screen()
+screen.exitonclick()
